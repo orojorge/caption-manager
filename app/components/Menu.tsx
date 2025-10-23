8f75305b96095c908f559eb71be002ebab7b7463
jorge <jorge@marksearch.online>
2025-10-23 10:58:23 +0200

    task page implemented

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -2,18 +2,20 @@
 
 import Link from "next/link";
 import DatasetList from './DatasetList';
-import Profiles from './Profiles';
 import ProfilesList from './ProfilesList';
 
 export default function Menu() {
   return (
     <aside className="w-full max-w-xs h-screen overflow-y-auto bg-white p-4">
-      <div className="mb-4 text-xl font-semibold text-gray-800">
+      <div className="mb-4 text-xl font-semibold">
         <Link href="/">Sartiq.ai</Link>
       </div>
       <div className="space-y-4">
         <DatasetList />
         <ProfilesList />
+        <h2 className="mb-3 text-lg font-semibold">
+          <Link href="/tasks">Tasks</Link>
+        </h2>
       </div>
     </aside>
   );
